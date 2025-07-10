8f0bbc2f08016ff8cbae9c51223ea45ab2920093
kdiffin <vhiunese@gmail.com>
2025-07-10 13:15:56 +0400

    wip

---
+++ internal/db/migrations/000001_create_initial_tables.down.sql
@@ -0,0 +1,15 @@
+DROP INDEX IF EXISTS song_playlist_rel_idx;
+
+DROP TABLE IF EXISTS song_playlist_rel;
+
+DROP INDEX IF EXISTS song_idx;
+
+DROP TABLE IF EXISTS songs;
+
+DROP INDEX IF EXISTS playlist_idx;
+
+DROP TABLE IF EXISTS playlists;
+
+DROP TABLE IF EXISTS sessions;
+
+DROP TABLE IF EXISTS users;
--- internal/db/migrations/000001_create_initial_tables.up.sql
+++ internal/db/migrations/000001_create_initial_tables.up.sql
@@ -17,22 +17,26 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    playlist (
+    playlists (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         createdAt TIMESTAMP NOT NULL DEFAULT now (),
         updated_at TIMESTAMP NOT NULL DEFAULT now (),
         name TEXT NOT NULL,
+        description TEXT,
         genre TEXT,
         pictureUrl TEXT,
-        authorName TEXT,
+        likes INT4,
+        dislikes INT4,
+        saves INT4,
+        authorId UUID REFERENCES users (id),
         -- no dude with the same two playlists
         UNIQUE (authorName, name)
     );
 
-CREATE UNIQUE INDEX playlist_idx ON songs (authorName, name);
+CREATE UNIQUE INDEX playlist_idx ON playlists (authorName, name);
 
 CREATE TABLE
-    song (
+    songs (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
         createdAt TIMESTAMP NOT NULL DEFAULT now (),
         updated_at TIMESTAMP NOT NULL DEFAULT now (),
@@ -40,13 +44,22 @@ CREATE TABLE
         pictureUrl TEXT,
         songUrl TEXT,
         genre TEXT,
-        rating INT4,
+        likes INT4,
+        dislikes INT4,
         artist TEXT,
         album TEXT,
         description TEXT,
         authorName TEXT,
-        playlistId UUID REFERENCES playlist (id),
         options JSONB
     );
 
-CREATE UNIQUE INDEX song_idx ON songs (authorName, name, playlistId);
+CREATE UNIQUE INDEX song_idx ON songs (authorName, name);
+
+CREATE TABLE
+    song_playlist_rel (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        song_id UUID REFERENCES songs (id),
+        playlist_id UUID REFERENCES playlists (id)
+    );
+
+CREATE UNIQUE INDEX song_playlist_rel_idx ON song_playlist_rel (song_id, playlist_id);
